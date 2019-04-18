94b27cfbbb8c81015f381f76ad5e9b53465fd719
kashyapSheladiya <kashyap@gmail.com>
2019-04-18 16:05:46 +0200

    Updated signIn page and authentication methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    helper_method :logged_in?, :current_user
+    
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+    
+    def logged_in?
+        !!current_user
+    end
+
+    def required_user
+        if !logged_in?
+            flash[:danger] = "You must be logged in to perform that action."
+            redirect_to root_path
+        end
+    end
 end
