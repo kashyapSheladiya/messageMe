7694037d29d63a07d82fa91e82394c1043a1cc59
kashyapSheladiya <kashyap@gmail.com>
2019-04-18 12:41:43 +0200

    Message model and messages table created

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
     validates :username, presence: true, 
               uniqueness: {case_sensitive: false},
               length: {minimum: 5, maximum:15}
-    
+    has_many :messages
     has_secure_password
 
 end
