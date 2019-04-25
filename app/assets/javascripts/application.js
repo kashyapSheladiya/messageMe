a529e5c8c52afde5c170f4f40563206b55e31091
kashyapSheladiya <kashyap@gmail.com>
2019-04-25 11:45:28 +0200

    Auto Scrolling to chat window

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,10 +17,17 @@
 //= require semantic-ui
 //= require_tree .
 
+scroll_bottom = function() {
+    if ($('#messages-scroll').length > 0) {
+        $('#messages-scroll').scrollTop($('#messages-scroll')[0].scrollHeight);
+    }
+}
+
 $(document).on('turbolinks:load', function() {
     $('.ui.dropdown').dropdown();
     $('.message .close').on('click', function() {
         $(this).closest('.message').transition('fade');
     });
+    scroll_bottom()
 })
 
